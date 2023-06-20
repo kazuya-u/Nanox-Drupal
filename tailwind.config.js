a195efff6185c10cdf19c724092ea87e3abc8db9
kazuya-umeki <kazuyaumeki1115@gmail.com>
2023-06-20 09:17:54 +0900

    Set Tailwindcss.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./web/themes/custom/lion/templates/**/*.html.twig",
+    "./web/themes/custom/lion/src/**/*.php",
+  ],
   theme: {
     extend: {},
   },
+  variants: {},
   plugins: [],
-}
+};
+
 
