d15a2e9243157d374050954faab3e6ba5f1b5954
kazuya-umeki <kazuyaumeki1115@gmail.com>
2024-02-21 00:03:13 +0900

    Implemented confirm form.

--- web/modules/custom/chatwork_api/src/Form/ChatworkForm.php
+++ web/modules/custom/chatwork_api/src/Form/ChatworkForm.php
@@ -4,6 +4,9 @@ namespace Drupal\chatwork_api\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\TempStore\PrivateTempStoreFactory;
+use Drupal\Core\Url;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a Chatwork API form.
@@ -11,56 +14,67 @@ use Drupal\Core\Form\FormStateInterface;
 final class ChatworkForm extends FormBase {
 
   /**
+   * The private tmp store.
+   */
+  protected PrivateTempStoreFactory $privateTempStore;
+
+  /**
+   * {@inheritDoc}
+   */
+  public function __construct(
+    PrivateTempStoreFactory $private_temp_store
+  ) {
+    $this->privateTempStore = $private_temp_store;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('tempstore.private'),
+    );
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function getFormId(): string {
-    return 'chatwork_api_chatwork';
+    return 'chatwork_api_form';
   }
 
   /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state): array {
-
-    $form['message'] = [
-      '#type' => 'textarea',
-      '#title' => $this->t('Message'),
-      '#required' => TRUE,
-    ];
-
-    $form['actions'] = [
-      '#type' => 'actions',
-      'submit' => [
+    $form = [
+      'message' => [
+        '#type' => 'textarea',
+        '#title' => $this->t('Message'),
+        '#required' => TRUE,
+      ],
+      'actions' => [
         '#type' => 'submit',
-        '#value' => $this->t('Send'),
+        '#value' => $this->t('確認'),
+        '#attributes' => [
+          'class' => ['button'],
+        ],
+        '#ajax' => [
+          'callback' => '::showConfirmationForm',
+          'event' => 'click',
+        ],
       ],
     ];
-
     return $form;
   }
 
   /**
    * {@inheritdoc}
    */
-  public function validateForm(array &$form, FormStateInterface $form_state): void {
-    // @todo Validate the form here.
-    // Example:
-    // @code
-    //   if (mb_strlen($form_state->getValue('message')) < 10) {
-    //     $form_state->setErrorByName(
-    //       'message',
-    //       $this->t('Message should be at least 10 characters.'),
-    //     );
-    //   }
-    // @endcode
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function submitForm(array &$form, FormStateInterface $form_state): void {
-    $this->messenger()->addStatus($this->t('The message has been sent.'));
-    $form_state->setRedirect('<front>');
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $tempstore = $this->privateTempStore->get('chatwork_api');
+    $tempstore->set('form_values', $form_state->getValues());
+    $form_state->setRedirectUrl(Url::fromRoute('chatwork_api.confirm'));
   }
 
 }
